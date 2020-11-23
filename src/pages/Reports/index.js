ba422bebe8a2f7cac01249afac5dc682cb6ce9cf
Samuel Felipe <samfelgar@gmail.com>
2020-11-23 01:59:42 -0300

    Reports main page and items by class report.

--- src/pages/Reports/index.js
+++ src/pages/Reports/index.js
@@ -1,16 +1,8 @@
 import React, {useEffect, useState} from 'react'
 import {useParams, useHistory} from 'react-router-dom'
 import Container from "@material-ui/core/Container";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper'
-import api from './../../services/api'
 import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
 
 const Reports = () => {
 
